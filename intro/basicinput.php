9b5d369d0193d48288ad5d049e2632b3c6ef4acf
Meija Lohiniva <mlohiniva@hotmail.com>
2018-01-10 15:35:56 +0200

    Changed the folder name

---
+++ intro/basicinput.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Workshop 1</title>
+	</head>
+	<body>
+		<form action="basicinputresponse.php" method="post">
+			Enter your first name:<br>
+			<input type="text" name="firstname">
+			<br>
+			<br>
+	  
+			Enter your last name:<br>
+			<input type="text" name="lastname">
+			<br>
+			<br>
+			
+			<input type="submit" value="Send input!">
+		</form>
+</body>
+</html>
+
+
+
+
