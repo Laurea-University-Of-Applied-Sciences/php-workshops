f6e9c689d06be3e1c5bc93d0e0a50c49816ded5b
Meija Lohiniva <mlohiniva@hotmail.com>
2018-03-07 14:01:12 +0200

    Initial commit

---
+++ Session App/dbinfo.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'yt4Wvs67_jer335');
+  define('DB_PASSWORD', 'PE3GXn3qJpwEvybr');
+  define('DB_NAME', 'session_db');
+?>
