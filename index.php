73cf0a12978421ece801b0b78bd60ef7680e5ec9
Meija Lohiniva <mlohiniva@hotmail.com>
2018-11-26 14:58:24 +0200

    Added .user.ini

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: MEILOHI
- * Date: 23.11.2018
- * Time: 10.34
- */
-    $dir    = getcwd (  );
-    $files = scandir($dir, 1);
-    $html = '<ul>';
-    foreach ($files as $key => $value) {
-        if(is_dir($value) && substr($value, 0,1 )!=".") {
-            $html .= '<li><a href="' . $value . '">' . $value . '</a></li>';
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>R0315 - Web App Development with PHP/SQL - Code for Hands on Exercises</h1>
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: MEILOHI
+     * Date: 23.11.2018
+     * Time: 10.34
+     */
+        $dir    = getcwd (  );
+        $files = scandir($dir, 1);
+        $html = '<ul>';
+        foreach ($files as $key => $value) {
+            if(is_dir($value) && substr($value, 0,1 )!=".") {
+                $html .= '<li><a href="' . $value . '">' . $value . '</a></li>';
+            }
         }
-    }
-    $html .= '</ul>';
-    echo $html;
+        $html .= '</ul>';
+        echo $html;
+    ?>
+</body>
+</html>
