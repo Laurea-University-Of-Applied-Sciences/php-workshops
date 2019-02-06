5fe912c930edab0129e50185992a6cdc96861bc0
Meija Lohiniva <mlohiniva@hotmail.com>
2019-02-06 12:03:45 +0200

    Fixed indentation and moved scripts to body

--- Self-processingFormsBootstrap/calculator-bs1.php
+++ Self-processingFormsBootstrap/calculator-bs1.php
@@ -23,39 +23,37 @@
 <!-- HTML FORM. This section of the file defines the HTML form that displays
 the calculator. The HTML code below will always be processed, independently if this file is executed via a POST method or not. -->
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Simple Calculator</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
- 
-<body>
-<div class="container">
+    <head>
+        <meta charset="utf-8">
+        <title>Simple Calculator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <h2> Simple Calculator </h2>
+            <!-- In a self-processing form, the action URL points to this same file, i.e $_SERVER['PHP_SELF'] -->
+            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+                Enter a value for X:<br>
+                <!-- Pay attention to the embedded PHP code in the next line. What is it actually doing? -->
+                <input type="text" name="valuex" value="<?php echo !empty($result)?$_POST['valuex']:'';?>">
+                <br><br>
 
-	<h2> Simple Calculator </h2> 
-	<!-- In a self-processing form, the action URL points to this same file, i.e $_SERVER['PHP_SELF'] -->
-	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-		Enter a value for X:<br>
-		<!-- Pay attention to the embedded PHP code in the next line. What is it actually doing? -->
-		<input type="text" name="valuex" value="<?php echo !empty($result)?$_POST['valuex']:'';?>">
- 		<br><br>
-  
-  		Enter a value for Y:<br>
-  		<!-- Here again, we use the same PHP code. As this is a self-processing form the script can remember the values the user entered before pressing the submit button. This way the values remain visible in the form until the user presses 'Clear' -->
-		<input type="text" name="valuey" value="<?php echo !empty($result)?$_POST['valuey']:'';?>">
-  		<br><br>
-  		
-		Result: <?php echo !empty($result)?$result:'';?>
-  		<br><br>
-		
-		<input type="submit" name="adding" value="X+Y">
-		<input type="submit" name="subtracting" value="X-Y">
-		<input type="submit" name="clearing" value="Clear">
-	</form>
+                Enter a value for Y:<br>
+                <!-- Here again, we use the same PHP code. As this is a self-processing form the script can remember the values the user entered before pressing the submit button. This way the values remain visible in the form until the user presses 'Clear' -->
+                <input type="text" name="valuey" value="<?php echo !empty($result)?$_POST['valuey']:'';?>">
+                <br><br>
 
-</div>
-</body>
+                Result: <?php echo !empty($result)?$result:'';?>
+                <br><br>
+
+                <input type="submit" name="adding" value="X+Y">
+                <input type="submit" name="subtracting" value="X-Y">
+                <input type="submit" name="clearing" value="Clear">
+            </form>
+
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
 </html>
