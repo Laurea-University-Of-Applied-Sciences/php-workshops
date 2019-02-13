e9d71652a3de02cbdae78023e128bac37c43b018
Meija Lohiniva <mlohiniva@hotmail.com>
2019-02-13 13:31:17 +0200

    Test

--- DB/list.php
+++ DB/list.php
@@ -13,6 +13,16 @@
     }
     echo "Connected successfully";
 
-    $sql = "SELECT * FROM testtable"
+    $sql = "SELECT * FROM testtable";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["Name"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
 
 ?>
