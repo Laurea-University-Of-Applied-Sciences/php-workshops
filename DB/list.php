ea7dddc84ad42ad5db9c6188060c7d80d02e26b5
Meija Lohiniva <mlohiniva@hotmail.com>
2019-02-13 13:51:29 +0200

    Working example

--- DB/list.php
+++ DB/list.php
@@ -12,7 +12,7 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
+    echo "Connected successfully<br/>";
 
     $sql = "SELECT * FROM testtable";
     $result = $conn->query($sql);
