45699298dc46ff73e1fd0ffaa3eacad8b1a62c27
Meija Lohiniva <mlohiniva@hotmail.com>
2019-02-13 13:07:58 +0200

    Test

--- DB/list.php
+++ DB/list.php
@@ -1,38 +1,27 @@
 <?php
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
+    $dbhost = '127.0.0.1:50818';
+    $dbname = 'localdb';
+    $dbusername = 'azure';
+    $dbpassword = '6#vWHD_$';
 
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
+    $connection = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
+
+    if (!$connection) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
     }
+    $sql = "SELECT * FROM testtable";
+    $result = $connection->query($sql);
 
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-$connection = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-
-if (!$connection) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-$sql = "SELECT * FROM testtable";
-$result = $connection->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["Name"]. "<br>";
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["Name"]. "<br>";
+        }
+    } else {
+        echo "0 results";
     }
-} else {
-    echo "0 results";
-}
-mysqli_close($connection);
+    mysqli_close($connection);
 ?>
