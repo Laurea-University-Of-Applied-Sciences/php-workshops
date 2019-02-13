70218113987bc0273a409f765b922af5ce482c32
Meija Lohiniva <mlohiniva@hotmail.com>
2019-02-13 13:22:16 +0200

    Test

--- DB/list.php
+++ DB/list.php
@@ -1,3 +1,16 @@
 <?php
 
+    $servername = "127.0.0.1:50818";
+    $username = "azure";
+    $password = "6#vWHD_$";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
 ?>
